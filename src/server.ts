4cf2a15a9d0540527424e2cc0baf27680778e2f2
לינוי פסינקוב <linoypasinkov@gmail.com>
2025-03-08 12:35:36 +0200

    add express configuration

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express, { Express } from "express";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(express.json());
+
+
+const initApp = () => {
+  return new Promise<Express>((resolve, reject) => {
+    if (!process.env.DB_CONNECT) {
+      reject("DB_CONNECT is not defined in .env file");
+    } else {
+      mongoose
+        .connect(process.env.DB_CONNECT)
+        .then(() => {
+          resolve(app);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    }
+  });
+};
+
+export default initApp;
