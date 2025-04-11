635c841cb0a49107c40a0758528ca620d99c49ab
ValeriyaLevinson <valeriya312002@gmail.com>
2025-04-11 19:28:36 +0300

    Add mongo db connection

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import mongoose from "mongoose";
 import siteInfoRoutes from './routes/siteInfo_route';
 import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUI from "swagger-ui-express";
+import bodyParser from "body-parser";
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/public", express.static("public"));
 app.use('/site-info', siteInfoRoutes);
@@ -29,8 +32,20 @@ const specs = swaggerJsDoc(options);
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 const initApp = () => {
-  return new Promise<Express>((resolve) => {
-    resolve(app);
+  return new Promise<Express>((resolve, reject) => {
+    if (!process.env.DB_CONNECT) {
+      reject("DB_CONNECT is not defined in .env file");
+    } else {
+      mongoose
+        .connect(process.env.DB_CONNECT as string)
+        .then(() => {
+          console.log("Connected to MongoDB");
+          resolve(app);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    }
   });
 };
 
