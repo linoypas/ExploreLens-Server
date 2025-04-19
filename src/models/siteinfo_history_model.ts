b008750c86d2cc510b619beb087a41e1d4a23fc4
ValeriyaLevinson <valeriya312002@gmail.com>
2025-04-19 15:07:17 +0300

    Create siteInfo history model, controller and route

---
+++ src/models/siteinfo_history_model.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+export interface ISiteInfoHistory {
+  siteInfoId: string;               
+  geohash: string;           
+  userId: string;            
+  createdAt?: Date;          
+  _id?: string;              
+}
+
+const siteInfoHistorySchema = new mongoose.Schema<ISiteInfoHistory>({
+  siteInfoId: {
+    type: String,
+    required: true
+  },
+  geohash: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const siteInfoHistoryModel = mongoose.model<ISiteInfoHistory>("SiteInfoHistory", siteInfoHistorySchema);
+
+export default siteInfoHistoryModel;
