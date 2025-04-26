cd8f0f4c8e0ccac1a22023062b3c256afd23a4d9
linoypas <linoypasinkov@gmail.com>
2025-04-26 14:37:07 +0300

    add some changes to deployment

--- src/site-detection/providers/prompts.ts
+++
@@ -1,2 +0,0 @@
-export const GptPromptForSystem= 'You are a helpful assistant that provides detailed information about landmarks and famous sites.'
-export const GptPromptForUser= 'Please provide short paragraph of detailed information about the landmark called'
--- src/test/scripts/providers/testObjectDetection.ts
+++
@@ -1,9 +0,0 @@
-import { detectObjects } from "../../../site-detection/providers/eden_ai/objectDetection_provider";
-
-const imagePath = "..\\KikarBialik.jpg";
-
-detectObjects(imagePath)
-  .then((objects) => {
-    console.log("Detected Objects:", objects);
-  })
-  .catch((error) => console.error("Unexpected Error:", error));
