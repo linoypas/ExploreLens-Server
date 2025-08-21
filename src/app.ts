c2fd04f13bd4a540ecd5221a5605ea47fbf1e9f7
ValeriyaLevinson <76559417+ValeriyaLevinson@users.noreply.github.com>
2025-08-21 19:11:13 +0300

    Revert "Deployment"

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,10 @@
 import initApp from "./server";
 const port = process.env.PORT;
-import http from 'http';
-import https from 'https';
-import fs from "fs";
-
 
 initApp().then((app) => {
-  if(process.env.NODE_ENV!=='production'){
-    console.log('development');
-    http.createServer(app).listen(process.env.PORT);
-  } else{
-    console.log('production')
-    const options = {
-      key: fs.readFileSync('../certs/myserver.key'),
-      cert: fs.readFileSync('../certs/CSB.crt')
-    };
-  https.createServer(options, app).listen(process.env.PORT);
-  }
-  }).catch((err) => {
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+}).catch((err) => {
     console.error(err);
     });
-
--- src/server.ts
+++ src/server.ts
@@ -41,7 +41,7 @@ const options = {
       version: "1.0.0",
       description: "REST server including authentication using JWT",
     },
-    servers: [{ url: `http://10.10.248.134:${PORT}` },{ url: `https://explorelensserver.cs.colman.ac.il:${PORT}` }],
+    servers: [{ url: `http://localhost:${PORT}` }],
   },
   apis: ["./src/routes/*.ts" , "./src/chat/routes/*.ts"],
 };
