198e13aa093a0cbb73bfda2d3ed1b337f021e5a2
ValeriyaLevinson <valeriya312002@gmail.com>
2025-08-22 11:31:55 +0300

    Revert "Merge pull request #48 from linoypas/revert-46-deployment"
    This reverts commit fe8b360fa8406f85bfed4bfe7e84e1f92ccea30c, reversing
    changes made to 0b18206139d86312b29713c2e36dd983ae6e6f96.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import initApp from "./server";
 const port = process.env.PORT;
+import http from 'http';
+import https from 'https';
+import fs from "fs";
+
 
 initApp().then((app) => {
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  });
-}).catch((err) => {
+  if(process.env.NODE_ENV!=='production'){
+    console.log('development');
+    http.createServer(app).listen(process.env.PORT);
+  } else{
+    console.log('production')
+    const options = {
+      key: fs.readFileSync('../certs/myserver.key'),
+      cert: fs.readFileSync('../certs/CSB.crt')
+    };
+  https.createServer(options, app).listen(process.env.PORT);
+  }
+  }).catch((err) => {
     console.error(err);
     });
+
